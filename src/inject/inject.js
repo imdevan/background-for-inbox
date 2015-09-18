7131642f6c7ccf11c74cd8d86258ce2c69630c29
Devan Huapaya <huapayadevan@gmail.com>
2015-09-18 02:04:09 -0500

    Changes background of done inbox. Cleaned up code

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,52 +1,54 @@
-
 window.bgImgObj = new Image();
+var bg;
 
 // When settings are changed
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-
 	// Reload the webpage
 	location.reload();
 });
 
-
-// Set background image of inbox
-function setBgImage(bgImg){
-
-	if (bgImg.length > 0) {
-		bgImgObj.onload = function(){
-			placeBg();	
-			document.body.style.backgroundImage ="url('" + bgImg + "')";
-	    };
-		
-		bgImgObj.src = bgImg;
-	
-	    document.body.style.backgroundRepeat ="no-repeat";
-	    document.body.style.backgroundAttachment ="fixed";
-	    
-
-
+function setDoneBG (bg) {
 	    // Change background when inbox is empty
-	    var elements2 = document.getElementsByClassName("av");
-	    for (var i = 0; i < elements2.length; i++) {
-	        elements2[i].style.backgroundImage ="url('" + bgImg + "')";
-	        elements2[i].style.backgroundRepeat ="no-repeat";
-	        elements2[i].style.backgroundSize ="cover";
+	    var doneBG = document.querySelector("#oB > div > div > div.I > div");
+
+	    if(bg.img.length > 0) {
+	        doneBG.style.backgroundImage ="url('" + bg.img + "')";
+	        doneBG.style.backgroundRepeat ="no-repeat";
+	        doneBG.style.backgroundSize ="cover";
+    	}
+
+    	if(bg.color.length > 0) {
+	        doneBG.style.backgroundColor = bg.color;
 	    }
-	}
+
+    	if(bg.color.length > 0 || bg.img.length > 0) {
+		    var doneSunBG = document.querySelector("#oB > div > div > div.I > div > div");
+		    doneSunBG.style.display = "none";
+		}
+
 }
 
-// Set background color of inbox
-function setBgColor(bgColor){
+setInterval(function(){ setDoneBG(bg) }, 50);
+
+// Set background image of inbox
+function setBackground (bg) {
+	if (bg.img.length > 0) {
+		bgImgObj.onload = function(){
+			document.body.style.backgroundImage ="url('" + bg.img + "')";
+	    };
+
+		bgImgObj.src = bg.img;
+
+	    document.body.style.backgroundRepeat ="no-repeat";
+	    document.body.style.backgroundAttachment ="fixed";
+	}
 
 	// Change body background
-	if(bgColor.length > 0){
-		document.body.style.backgroundColor = bgColor;
-
-	    // Change background when inbox is empty
-	    var elements2 = document.getElementsByClassName("av");
-	    for (var i = 0; i < elements2.length; i++) 
-			elements2[i].style.backgroundColor = color;
+	if(bg.color.length > 0){
+		document.body.style.backgroundColor = bg.color;
 	}
+
+	setDoneBG(bg);
 }
 
 // the grey bars above sections
@@ -57,88 +59,21 @@ function removeDayBackgrounds(){
 	}
 }
 
-// place body background position
-function placeBg(){
-	
-	// start with full width for display size
-	bgImgObj.displayWidth = window.innerWidth;
-	bgImgObj.displayHeight = 	bgImgObj.displayWidth *
-								bgImgObj.height /
-								bgImgObj.width;
-
-	if(bgImgObj.displayHeight >= window.innerHeight){
-		// is full width and scrolling
-		
-		//scroll offset 
-		var posY = (0-window.scrollY)*bgImgObj.Parallax;
-		//max out on scroll down
-		if(posY < -1 * (bgImgObj.displayHeight - window.innerHeight) ){
-			posY = -1 * (bgImgObj.displayHeight - window.innerHeight);
-		}
-		//min on scroll up
-		if(posY > 0){
-			posY = 0;
-		}
-		// set resolved position 
-		document.body.style.backgroundPositionY = posY + "px";
-		document.body.style.backgroundPositionX = "0px";
-	}else{
-		// is full height and centered
-		bgImgObj.displayHeight = window.innerHeight;
-		bgImgObj.displayWidth = bgImgObj.displayHeight *
-								bgImgObj.width /
-								bgImgObj.height;
-
-		// find x that centers the background
-		var newX = window.width - bgImgObj.displayWidth;
-			newX /= 2;
-
-		//set the resolved position
-		document.body.style.backgroundPositionY = "0px";
-		document.body.style.backgroundPositionX = newX + "px";
-	}
-	//set the resolved size 
-	document.body.style.backgroundSize = bgImgObj.displayWidth+"px "+ bgImgObj.displayHeight+"px";
-}
-
+// Get data from chrome storage
 chrome.extension.sendMessage({}, function(response) {
-
 	var readyStateCheckInterval = setInterval(function() {
 
 		if (document.readyState === "complete") {
 
-			removeDayBackgrounds();
-
-			var bgColor = "", bgImage = "";
 			clearInterval(readyStateCheckInterval);
-
-
+			document.body.style.WebkitTransition = 'background-color 2s ease-in-out, background-image 2s ease-in-out';
 			// Get color from chrome storage
-			chrome.storage.sync.get("bg-for-inbox-color",function(bgColorFromStore){
-				for(key in bgColorFromStore){
-					bgColor = bgColorFromStore[key];
-
-					setBgColor(bgColor); 
+			chrome.storage.sync.get("background-for-inbox",function (data){
+				for(key in data){
+					bg = data[key];
+					setBackground(bg); 
 				}
 			});
-
-			// Get image link from chrome storage
-			chrome.storage.sync.get("bg-for-inbox-img",function(bgImgFromStore){
-				for(key in bgImgFromStore){
-					bgImage = bgImgFromStore[key];
-
-					setBgImage(bgImage);
-				}
-			});
-
-			chrome.storage.sync.get("bg-for-inbox-pr",function(bgPrFromStore){
-				window.bgImgObj.Parallax = bgPrFromStore["bg-for-inbox-pr"];
-			});
-
-			//set listener for scrolling
-			window.onscroll = placeBg;
-			window.onresize = placeBg;
-			
 		}
 	}, 10);
 });
--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -1,53 +1,27 @@
 
-// Get color from chrome storage
-chrome.storage.sync.get("bg-for-inbox-color",function(bgColorFromStore){
-	for(key in bgColorFromStore){
-		var bgColor = bgColorFromStore[key];
-		
-		// Set inbox value in page action
-		document.getElementById("bg-color-input").value = bgColor;
-	}
-});
-
-// Get image link from chrome storage
-chrome.storage.sync.get("bg-for-inbox-img",function(bgImgFromStore){
-	for(key in bgImgFromStore){
-		var bgImage = bgImgFromStore[key];
+// Get background info from chrome storage
+chrome.storage.sync.get("background-for-inbox",function(bgFromStore){
+	for(key in bgFromStore){
+		var bg = bgFromStore[key];
 
 		// Set inbox value in page action
-		document.getElementById("bg-img-input").value = bgImage;
+		document.getElementById("bg-color-input").value = bg.color;
+		document.getElementById("bg-img-input").value = bg.img;
 	}
 });
-
-chrome.storage.sync.get("bg-for-inbox-pr",function(bgPrFromStore){
-	console.log(bgPrFromStore);
-	var bgPr = bgPrFromStore["bg-for-inbox-pr"];
-
-	document.getElementById("range-output").innerHTML = "Parallax: "+bgPr*100 + "%";
-	document.getElementById("bg-pr-input").value = bgPr;
-});
-
 
 function updateBtnClicked(){
-	var bgColorSet = document.getElementById("bg-color-input").value;
-	var bgImgSet = document.getElementById("bg-img-input").value;
-	
-	chrome.storage.sync.set({"bg-for-inbox-color": bgColorSet},function(){});
-	chrome.storage.sync.set({"bg-for-inbox-img": bgImgSet},function(){});
+	var bg = {
+		color: document.getElementById("bg-color-input").value,
+		img: document.getElementById("bg-img-input").value
+	}
+
+	chrome.storage.sync.set({"background-for-inbox": bg},function(){});
 };
 
 function resetBtnClicked(){
-	chrome.storage.sync.set({"bg-for-inbox-color": ""},function(){});
-	chrome.storage.sync.set({"bg-for-inbox-img": ""},function(){});
+	chrome.storage.sync.set({"background-for-inbox": null},function(){});
 };
-
-document.getElementById("bg-pr-input").addEventListener("change",function(){
-	
-	var val = this.value;
-	chrome.storage.sync.set({"bg-for-inbox-pr": val},function(){
-		document.getElementById("range-output").innerHTML = "Parallax: "+val*100 + "%";	
-	});
-}, false);
 
 document.getElementById("update-button").addEventListener("click", updateBtnClicked, false);
 document.getElementById("reset-button").addEventListener("click", resetBtnClicked, false);
